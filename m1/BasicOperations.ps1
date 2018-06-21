d5399628bbd65f25dc7f2b4950b34f30d38914b9
Ned Bellavance <ned.bellavance@gmail.com>
2018-06-20 22:32:55 -0400

    module 1

--- m1/BasicOperations.ps1
+++ m1/BasicOperations.ps1
@@ -14,5 +14,23 @@ Get-AzureADTenantDetail
 
 Get-AzureADDomain
 
+#Working with roles
+Get-AzureADDirectoryRole
+
+$CompanyAdminRole = Get-AzureADDirectoryRole | Where-Object {$_.DisplayName -eq "Company Administrator"}
+
+Get-AzureADDirectoryRoleMember -ObjectId $CompanyAdminRole.ObjectId
+
+Get-AzureADDirectoryRoleTemplate
+
+$BillingAdminRoleTemplate = Get-AzureADDirectoryRoleTemplate | Where-Object {$_.DisplayName -eq "Helpdesk Administrator"}
+
+#Activate the role
+$BillingAdminRole = Enable-AzureADDirectoryRole -RoleTemplateId $BillingAdminRoleTemplate.ObjectId
+
+$user = Get-AzureADUser -Top 1
+
+Add-AzureADDirectoryRoleMember -RefObjectId $user.ObjectId -ObjectId $BillingAdminRole.ObjectId
+
 
 
